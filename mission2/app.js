c6292963f81b154bdeb6cae3a446fd9c3362be8a
moaibad <fathulibad.412@gmail.com>
2023-08-30 01:31:34 +0700

    update

--- mission2/app.js
+++ mission2/app.js
@@ -31,16 +31,19 @@ document.addEventListener("DOMContentLoaded", function() {
                             <button class="btn btn-outline-primary" onclick="kurangQty('qty_${data.id}')">-</button>
                             <input class="text-center m-2" type="text" id="qty_${data.id}" value="0">
                             <button class="btn btn-outline-primary" onclick="tambahQty('qty_${data.id}')">+</button>
-                            <button class="add-to-cart btn btn-lg btn-block btn-primary" onClick = tambahKeranjang(data)>Add to cart</button>
+                            <button class="add-to-cart btn btn-lg btn-block btn-primary" onClick="tambahKeranjang('${data.name}', ${data.price}, '${data.id}')">Add to cart</button>
                         </div>
                     </div>
                 </div>
             `;
-    
+        
             return colDiv;
         }
 
+        showTotalHarga()
 });
+
+var totalHarga = 0;
 
 function kurangQty(qty_id) {
     const inputElement = document.getElementById(qty_id);
@@ -58,6 +61,41 @@ function tambahQty(qty_id) {
     inputElement.value = qty;
 }
 
-tambahKeranjang(data)
+function tambahKeranjang(name, price, id){
+    var listCart = document.getElementById("list-cart");
+    var qty = document.getElementById("qty_"+id);
+    
+    totalHarga += price * qty.value;
+    
+    const rowDiv = document.createElement("div");
+    rowDiv.className = "row";
+    rowDiv.innerHTML = `
+            <div class="col-6">
+                ${name}
+            </div>
+            <div class="col-2">
+                x${qty.value}
+            </div>
+            <div class="col-4">
+                Rp. ${(price*qty.value).toLocaleString("id-ID")}
+            </div>
+            <hr style="width:100%;">
+    `;
+
+    if(qty.value != 0){
+        showTotalHarga()
+        listCart.appendChild(rowDiv);
+    }
+    else{
+        alert("Please add quantity of the product ;)")
+    }
+    
+}
+
+function showTotalHarga() {
+    const hargaTotal = document.getElementById('total-harga');
+    hargaTotal.innerHTML = `<h5>Total Harga : Rp. ${totalHarga.toLocaleString("id-ID")}</h5>`;
+}
+
 
 
